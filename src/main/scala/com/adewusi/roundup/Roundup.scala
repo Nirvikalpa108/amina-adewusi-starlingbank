64e36af626e5443975da4e42de953da120998bff
Amina <amina.adewusi@gmail.com>
2025-08-27 10:11:49 +0100

    refactor: new design idea for storage

--- src/main/scala/com/adewusi/roundup/Roundup.scala
+++ src/main/scala/com/adewusi/roundup/Roundup.scala
@@ -1,5 +1,6 @@
 package com.adewusi.roundup
 
+import cats.Monad
 import cats.data.EitherT
 import cats.effect.Sync
 import com.adewusi.roundup.model._
@@ -13,12 +14,14 @@ object Roundup {
       startDate: LocalDate,
       config: AppConfig
   )(implicit
-      accountClient: AccountClient[F],
-      transactionClient: TransactionClient[F],
-      savingsGoalClient: SavingsGoalClient[F],
-      idempotencyService: IdempotencyClient[F],
-      accountSelector: AccountSelector,
-      transactionValidator: TransactionValidator
+    accountClient: AccountClient[F],
+    transactionClient: TransactionClient[F],
+    savingsGoalClient: SavingsGoalClient[F],
+    goalRepository: GoalRepository[F],
+    goalService: GoalService[F],
+    transferLedger: TransferRepository[F],
+    accountSelector: AccountSelector,
+    transactionValidator: TransactionValidator
   ): EitherT[F, AppError, Unit] = {
     for {
       accounts <- EitherT(accountClient.fetchAccounts)
@@ -26,41 +29,49 @@ object Roundup {
       transactions <- EitherT(transactionClient.fetchTransactions(account, startDate))
       validatedTransactions <- EitherT.fromEither(transactionValidator.validateTransactions(transactions))
       roundup <- EitherT.fromEither(transactionValidator.validateRoundupAmount(validatedTransactions))
-      goalUuid <- EitherT(savingsGoalClient.readGoalFromFile(config)).flatMap {
-        case Some(goal) =>
-          EitherT(savingsGoalClient.validateGoal(goal, account.accountUid)).map(_.savingsGoalUid)
-        case None =>
-          EitherT(savingsGoalClient.createAndPersistGoal(config, account.accountUid))
-      }
-      // NOTE: In production, idempotency + recording should be atomic to avoid race conditions.
-      // This test assumes single-instance execution.
-      needsProcessing <- EitherT(
-        idempotencyService.checkIdempotency(goalUuid, startDate, roundup)
-      )
-      _ <- EitherT.cond[F](
-        needsProcessing,
-        (),
-        AlreadyTransferred(s"Roundup for $startDate already processed")
-      )
-      transfer <- EitherT(savingsGoalClient.transferToSavingsGoal(config, account.accountUid, goalUuid, roundup))
-      _ <- EitherT(
-        idempotencyService.recordTransfer(goalUuid, startDate, roundup, transfer)
-      )
+      goalUuid <- EitherT(goalService.getOrCreateGoal(config, account.accountUid))
+      _ <- EitherT(goalRepository.persistGoal(goalUuid))
+      needsProcessing <- EitherT(transferLedger.isEligibleForTransfer(goalUuid, startDate, roundup))
+      _ <- EitherT.cond[F](needsProcessing, (), AlreadyTransferred(s"Roundup for $startDate already processed"))
+      transfer <- EitherT(savingsGoalClient.transferToGoal(account.accountUid, goalUuid, roundup))
+      _ <- EitherT(transferLedger.recordTransfer(goalUuid, startDate, roundup, transfer))
     } yield ()
   }
 }
 
-trait IdempotencyClient[F[_]] {
-  def checkIdempotency(
-      goal: UUID,
-      startDate: LocalDate,
-      amountMinorUnits: Long
-  ): F[Either[AppError, Boolean]]
-  def recordTransfer(
-      goal: UUID,
-      startDate: LocalDate,
-      amountMinorUnits: Long,
-      transfer: AddMoneyResponse
-  ): F[Either[AppError, Unit]]
+trait GoalRepository[F[_]] {
+  def readGoal(config: AppConfig): F[Either[AppError, Option[UUID]]]
+  def persistGoal(goal: UUID): F[Either[AppError, Unit]]
 }
 
+trait TransferRepository[F[_]] {
+  def isEligibleForTransfer(goal: UUID, startDate: LocalDate, amountMinorUnits: Long): F[Either[AppError, Boolean]]
+  def recordTransfer(goal: UUID, startDate: LocalDate, amountMinorUnits: Long, transfer: AddMoneyResponse): F[Either[AppError, Unit]]
+}
+
+trait SavingsGoalClient[F[_]] {
+  def getGoal(goal: UUID, accountUid: UUID): F[Either[AppError, SavingsGoal]]
+  def createGoal(accountUid: UUID): F[Either[AppError, UUID]]
+  def transferToGoal(accountUid: UUID, goal: UUID, amountMinorUnits: Long): F[Either[AppError, AddMoneyResponse]]
+}
+
+trait GoalService[F[_]] {
+  def getOrCreateGoal(config: AppConfig, accountUid: UUID): F[Either[AppError, UUID]]
+}
+
+object GoalService {
+  def impl[F[_]: Monad](goalRepository: GoalRepository[F], savingsGoalClient: SavingsGoalClient[F]): GoalService[F] = new GoalService[F] {
+    def getOrCreateGoal(config: AppConfig, accountUid: UUID): F[Either[AppError, UUID]] = {
+      val result = for {
+        maybeGoal <- EitherT(goalRepository.readGoal(config))
+        goalId <- maybeGoal match {
+          case Some(goal) => EitherT(savingsGoalClient.getGoal(goal, accountUid)).map(_.savingsGoalUid)
+          case None       => EitherT(savingsGoalClient.createGoal(accountUid))
+        }
+        _ <- EitherT(goalRepository.persistGoal(goalId))
+      } yield goalId
+
+      result.value
+    }
+  }
+}
